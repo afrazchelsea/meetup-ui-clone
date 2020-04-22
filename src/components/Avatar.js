42e4c17a5ff01c82a85304d0f28815181612d241
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-04-22 23:49:51 +0530

    update

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,15 +1,19 @@
 const sizeMapping = {
-    sm:'w-8 h-8',
-    base: 'w-12 h-12'
+  sm: "w-8 h-8",
+  base: "w-12 h-12",
+  lg: "w-16 h-16",
+};
+
+function Avatar(props) {
+  const { size = "base", className, ...restProps } = props;
+
+  return (
+    <img
+      className={`rounded-full ${sizeMapping[size]} `}
+      src="https://secure.meetupstatic.com/photos/member/6/a/0/0/thumb_289767136.jpeg"
+      alt="avatar"
+    />
+  );
 }
 
-
-function Avatar(props){
-const {size='base', ...restProps} = props;
-
-    return 
-        <img className={`rounded-full ${sizeMapping[size]} `} src="https://secure.meetupstatic.com/photos/member/6/a/0/0/thumb_289767136.jpeg" alt="avatar"/>
-
-}
-
-export Avatar;
+export default Avatar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,97 @@
 import Head from "next/head";
-// import Avatar from "../components/Avatar.js";
+import Avatar from "../components/Avatar";
 
 const Home = () => (
   <div>
     <Head>
-      <title>Homepage | Aditya Agarwal</title>
+      <title>Meetup UI Clone | Afraz Momin</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <nav className="flex items-center justify-between py-4 px-8 border-b-2 border-gray-300">
       <div className="w-32">
-        <a href="https://www.meetup.com/">
+        <a href="https://www.meetup.com/" target="_blank">
           <img
-            src="https://secure.meetupstatic.com/s/img/5455565085016210254/logo/svg/logo--script.svg"
+            src="https://www.meetup.com/mu_static/en-US/logo--script.004ada05.svg"
             alt="meetup-logo"
           />
         </a>
       </div>
-      <div>
-        <div bg-color-blue-700>
-          <span>Group</span>
+      <div className="flex">
+        <div className="text-blue-600 font-bold text-center">
+          <span>Start a new group</span>
           <br />
           <span>50% off</span>
         </div>
         <div className="border-r border-gray-300 mx-4"></div>
         <div className="flex items-center">
-          <ul className="flex space-x-3 ">
+          <ul className="flex space-x-3 text-center text-gray-700 text-sm">
             <li>Explore</li>
             <li>Notifications</li>
             <li>Messages</li>
-            <li>{/* <Avatar size="sm" /> */}</li>
+            <li>
+              <Avatar size="sm" />
+            </li>
           </ul>
         </div>
       </div>
     </nav>
-    <header className="px-32">Header</header>
-    <main className="text-gray-900 px-32 bg-body flex space-x-32  pt-10 pb-16">
-      <div className="w-8/12">
-        <section className="mb-10">
-          <h4 className="font-medium text-xl mb-4">Details</h4>
-          <p className="mb-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            architecto explicabo iusto delectus labore natus beatae. Assumenda
-            ab non doloribus.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            architecto explica
-          </p>
-        </section>
-        <section className="mb-10">
-          <div className="flex justify-between">
-            <h4 className="font-medium text-xl mb-4">Attendees</h4>
-            <button className="text-blue-500">See all</button>
+    <header className="px-32 py-4 ">
+      <div>
+        <span className="text-gray-600">Saturday, May 16, 2020</span>
+        <h1 className="text-3xl font-black mb-4">react online meetup #02</h1>
+        <div>
+          <Avatar />
+          <div>
+            <p>Hosted by</p>
+            <h6 className="font-medium">Afraz Momin</h6>
           </div>
-          <ul className="grid gird-cols-4 gap-4">
-            <li className="bg-white rounded-lg flex flex-col items-center py-4  ">
-              {/* <Avatar /> */}
-              <h6 className="font-bold text-sm">Lawerance D</h6>
-              <p className="text-sm text-gray-600">Member</p>
-              <p className="text-sm text-gray-600">8 shared groups</p>
-            </li>
-          </ul>
-        </section>
+        </div>
+      </div>
+      <div>
+        <button className="text-gray-600 border border-gray-500 px-6 py-2 rounded-lg">
+          Share
+        </button>
+      </div>
+    </header>
+
+    <main className="text-gray-900 px-32 bg-body pt-10 pb-16">
+      <div className="flex space-x-32">
+        <div className="w-8/12">
+          <section className="mb-10">
+            <h4 className="font-medium text-xl mb-5">Details</h4>
+            <p className="mb-3">
+              This is online meetup which you can join with Zoom/Youtube Live.
+            </p>
+            <p>
+              If you like to present in this meetup propose a talk{" "}
+              <a
+                className="text-blue-500"
+                href="https://forms.gle/k5er77BpC5EDR4sE9"
+              >
+                https://forms.gle/k5er77BpC5EDR4sE9
+              </a>
+            </p>
+          </section>
+          <section className="mb-10">
+            <div className="flex justify-between">
+              <h4 className="font-medium text-xl mb-5">Attendees</h4>
+              <button className="text-blue-500">See all</button>
+            </div>
+            <ul className="grid grid-cols-4 gap-4">
+              {Array.from({ length: 8 }).map((_, index) => (
+                <li
+                  key={index}
+                  className="bg-white rounded-lg flex flex-col items-center py-4  "
+                >
+                  <Avatar />
+                  <h6 className="font-bold text-sm">Lawerance D</h6>
+                  <p className="text-sm text-gray-600">Member</p>
+                  <p className="text-sm text-gray-600">8 shared groups</p>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
       </div>
       <aside className="w-4/12"></aside>
     </main>
